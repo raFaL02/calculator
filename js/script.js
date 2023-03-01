bdeb4e3bf2bf9a1ec3c7a7dd27282ca546ca64a4
raFa <rafalimabcl@gmail.com>
2023-02-28 23:40:18 -0300

    add click e num no display

--- js/script.js
+++ js/script.js
@@ -1,1 +1,41 @@
+const calculator = document.querySelector("#calculator");
+const display = document.querySelector("#display");
+const lastCalcText = document.querySelector("#last-calc");
+const result = document.querySelector("#result");
+const keyboard = document.querySelectorAll("#keyboard");
 
+
+class CalculatorFun {
+    constructor(lastCalcText, result) {
+        this.lastCalcText = lastCalcText;
+        this.result = result;
+        this.lastCalc = "";
+    }
+
+    // add digit to calculator display
+    addDigit(digit) {
+        this.lastCalc = digit;
+        this.updateScreen();
+    }
+
+    // change values of the calculator display
+    updateScreen() {
+        this.lastCalcText.innerText += this.lastCalc
+    }
+}
+
+const calc = new CalculatorFun(lastCalcText, result);   
+
+keyboard.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+
+        const value = e.target.innerText;
+
+        if(+value >= 0 || value === ",") {
+            calc.addDigit(value);
+        }else {
+            console.log("Op" + value);
+        }
+
+    });
+});
