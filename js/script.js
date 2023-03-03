596da0c041e33d2fce012e3f15a845c15ae92648
raFa <rafalimabcl@gmail.com>
2023-03-02 23:21:11 -0300

    Fazendo os numeros aparecer no display

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ const result = document.querySelector("#result");
 const keyboard = document.querySelectorAll("#keyboard");
 
 
-class CalculatorFun {
+class CalculatorFunc {
     constructor(lastCalcText, result) {
         this.lastCalcText = lastCalcText;
         this.result = result;
@@ -14,17 +14,52 @@ class CalculatorFun {
 
     // add digit to calculator display
     addDigit(digit) {
+    // check if current operation already has a dot
+    if(digit === "," && this.lastCalcText.innerText.includes(",")) {
+        return;
+    }   
+
         this.lastCalc = digit;
         this.updateScreen();
     }
 
+    // Process all calculator operation
+    processOperation(operation) {
+        // Get current and previous value
+        let operationValue; 
+        const previous = +this.lastCalcText.innerText;
+        const current = +this.result.innerText;
+
+        switch(operation) {
+            case "+":
+                operationValue = previous + current;
+                this.updateScreen(operationValue, operation, current, previous);
+                break;
+            default:
+                return;
+        }
+    }
+
     // change values of the calculator display
-    updateScreen() {
-        this.lastCalcText.innerText += this.lastCalc
+    updateScreen(operationValue = null, operation = null, current = null, previous = null) {
+        
+        if(operationValue === null) {
+            this.lastCalcText.innerText += this.lastCalc
+        }else {
+            // check if value is zero, if it is just add current value
+            if(previous === 0) {
+                operationValue = current;
+            }
+            
+            // add current value to previous
+            this.lastCalcText.innerText = `${operationValue} ${operation}`
+            this.result.innerText = "";
+        }
+
     }
 }
 
-const calc = new CalculatorFun(lastCalcText, result);   
+const calc = new CalculatorFunc(lastCalcText, result);   
 
 keyboard.forEach((btn) => {
     btn.addEventListener("click", (e) => {
@@ -34,7 +69,7 @@ keyboard.forEach((btn) => {
         if(+value >= 0 || value === ",") {
             calc.addDigit(value);
         }else {
-            console.log("Op" + value);
+            calc.processOperation(value);
         }
 
     });
